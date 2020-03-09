5280df7783efb2134ecb6ce63a6dfb6981c4d6b9
wjonke <wesjonke@gmail.com>
2020-03-09 15:50:55 -0400

    completed challenges 1-4

--- src/03_modules.py
+++ src/03_modules.py
@@ -1,3 +1,9 @@
+# method 1 to get host name
+import socket
+
+# method 2 to get host name
+from Scripts._socket import gethostname
+
 """
 In this exercise, you'll be playing around with the sys module,
 which allows you to access many system specific variables and
@@ -23,9 +29,26 @@ import os
 
 # Print the current process ID
 # YOUR CODE HERE
+print(os.getpid())
 
 # Print the current working directory (cwd):
 # YOUR CODE HERE
+print(os. getcwd())
 
 # Print out your machine's login name
 # YOUR CODE HERE
+
+# method 1 to get host name
+print(gethostname())
+
+
+# method 2 to get host name
+def get_Host_name_IP():
+    try:
+        host_name = socket.gethostname()
+        print("Hostname ----> :  ", host_name)
+    except:
+        print("Unable to get Hostname and IP")
+
+
+get_Host_name_IP()
