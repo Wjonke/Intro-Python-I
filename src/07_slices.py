4af660bb4ee9debfb203e0213e64193ecf6c34c9
wjonke <wesjonke@gmail.com>
2020-03-10 19:13:41 -0400

    finished challenge 7 questions and fixed up challenge 6 to reflect slices specifically

--- src/07_slices.py
+++ src/07_slices.py
@@ -8,26 +8,73 @@ overview: https://stackoverflow.com/a/509295
 
 Use Python's slice syntax to achieve the following:
 """
+import operator
 
 a = [2, 4, 1, 7, 9, 6]
 
 # Output the second element: 4:
-print()
+# Way 1
+x = slice(1, 2)
+print(a[x])
+
+
+# Way 2
+def convert(the_list):
+    return tuple(i for i in the_list)
+
+
+x = slice(1, 2)
+tuple_list = convert(a)
+print(tuple_list[x])
+
+# Way 3
+print(operator.itemgetter(1)(a))
 
 # Output the second-to-last element: 9
-print()
+# Way 1
+x = slice(len(a) - 2, len(a) - 1)
+print(a[x])
+
+# Way 2
+print(operator.itemgetter(-2)(a))
 
 # Output the last three elements in the array: [7, 9, 6]
-print()
+# Way 1
+x = slice(len(a) - 3, len(a))
+print(a[x])
+
+# Way 2
+print(list(operator.itemgetter(-3, -2, -1)(a)))
+
+
+
+
 
 # Output the two middle elements in the array: [1, 7]
-print()
+#will also return middle element of length is odd
+mid_index = int(len(a) / 2)
+print("mid_index", mid_index)
+
+if len(a) % 2 == 0:
+    x = slice(mid_index - 1, mid_index +1)
+
+elif len(a) % 2 != 0:
+    x = slice(mid_index, mid_index +1)
+
+print("middle of the list ---->", a[x])
+
+
 
 # Output every element except the first one: [4, 1, 7, 9, 6]
-print()
+
+x = slice(1, len(a))
+print(a[x])
+
 
 # Output every element except the last one: [2, 4, 1, 7, 9]
-print()
+x = slice(0, len(a)-1)
+print(a[x])
+
 
 # For string s...
 
